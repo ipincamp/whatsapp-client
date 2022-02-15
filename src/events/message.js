fac4a5ec62c6283afa66aa873815f71bd5852043
ipincamp <arif.lngsrn@gmail.com>
2022-02-15 22:48:49 +0700

    Dumping message into console

--- src/events/message.js
+++ src/events/message.js
@@ -5,18 +5,18 @@
  * @license Apache-2.0
  */
 
-const { Message } = require("whatsapp-web.js");
-const { prefixs } = require("../utils/Config");
+const { LOG } = require("../utils/Logger");
+const { MSG } = require("../utils/Messages");
 
 module.exports = {
   name: "message",
-  /**
-   *
-   * @param {Message} message
-   */
-  async execute(message) {
-    if (message.body == `${prefixs}hai`) {
-      message.reply("Apa?");
+  async run(message) {
+    if (message.body == `${MSG.prefix}`) {
+      message.reply(MSG.message.reply);
     }
+
+    const contact = await message.getContact();
+
+    LOG.info(`${contact.number} (${contact.name ? contact.name : contact.pushname}): ${message.body}`);
   },
 };
