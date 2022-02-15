55c055f5e80947b11bc0bdf24c2c4aaa8cb46f24
ipincamp <arif.lngsrn@gmail.com>
2022-02-15 22:20:32 +0700

    Remodule event listener

---
+++ src/utils/Events.js
@@ -0,0 +1,23 @@
+/**
+ * @name whatsapp-client
+ * @version v1.0.0
+ * @author ipincamp <support@nur-arifin.my.id>
+ * @license Apache-2.0
+ */
+
+const fs = require("fs");
+const { join } = require("path");
+
+module.exports = (client) => {
+  const eventFiles = fs.readdirSync(join(__dirname, "../events")).filter((f) => f.endsWith(".js"));
+
+  for (const file of eventFiles) {
+    const event = require(join(__dirname, "../events", `${file}`));
+
+    if (event.once) {
+      client.once(event.name, (...args) => event.run(...args));
+    } else {
+      client.on(event.name, (...args) => event.run(...args));
+    }
+  }
+};
