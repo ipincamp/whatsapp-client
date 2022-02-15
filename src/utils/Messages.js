c00f655b8c7a3649dd2035c1d83000251c68c5b9
ipincamp <arif.lngsrn@gmail.com>
2022-02-15 22:48:26 +0700

    Deprecated from Config.js
    1. It will use for locales. Hope easy to use.
    2. Export to global variable.

---
+++ src/utils/Messages.js
@@ -0,0 +1,22 @@
+/**
+ * @name whatsapp-client
+ * @version v1.0.0
+ * @author ipincamp <support@nur-arifin.my.id>
+ * @license Apache-2.0
+ */
+
+const messages = {
+  auth: {
+    fail: "Don't worry, creating new session...",
+    success: "Authentication success!.",
+  },
+  disconnect: "Client logout, make a new session...",
+  message: {
+    reply: "Pa pe pa pe, salam yang bener!",
+  },
+  prefix: "p",
+  qr: "Scan this code, it will be regenerated in 30 seconds",
+  ready: "Client is ready!",
+};
+
+module.exports.MSG = messages;
