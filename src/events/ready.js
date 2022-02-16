d63a9596a463b538a6471df1202041070d7fb1d4
ipincamp <arif.lngsrn@gmail.com>
2022-02-16 15:11:46 +0700

    Get cell phone information.

--- src/events/ready.js
+++ src/events/ready.js
@@ -5,6 +5,7 @@
  * @license Apache-2.0
  */
 
+const client = require("../..");
 const { LOG } = require("../utils/Logger");
 const { MSG } = require("../utils/Messages");
 
@@ -12,5 +13,20 @@ module.exports = {
   name: "ready",
   run() {
     LOG.info(MSG.ready);
+
+    try {
+      const device = client.info.phone;
+
+      const info = `Hardware Information:
+            Manufacturer     : ${device.device_manufacturer}
+            Model            : ${device.device_model}
+            OS Build         : ${device.os_build_number}
+            OS Version       : ${device.os_version}
+            WhatsApp Version : ${device.wa_version}
+      `;
+      LOG.info(info);
+    } catch (err) {
+      LOG.error(err);
+    }
   },
 };
