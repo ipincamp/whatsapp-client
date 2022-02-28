6881f1b83baadb9c7a71aae6b47b175f2eb75b6f
ipincamp <support@nur-arifin.my.id>
2022-02-28 12:42:55 +0700

    Remodule event

--- src/events/message.js
+++ src/events/message.js
@@ -5,28 +5,29 @@
  * @license Apache-2.0
  */
 
+const { Client } = require("whatsapp-web.js");
 const { LOG } = require("../utils/Logger");
 const { MSG } = require("../utils/Messages");
-
-module.exports = {
-  name: "message",
-  /**
-   *
-   * @param {import("whatsapp-web.js").Message} msg
-   */
-  async run(msg) {
-    if (msg.body === MSG.prefix || (msg.body === "P")) {
-      msg.reply(MSG.message.reply);
-    }
-
-    const name = await msg.getContact();
-
-    if (MSG.logging.enabled === true) {
-      try {
-        LOG.info(`${name.number} (${name.name ? name.name : name.pushname}): ${msg.body}`);
-      } catch (err) {
-        LOG.error(err);
+/**
+ *
+ * @param {Client} client
+ */
+module.exports = function (client) {
+  client.on(
+    "message",
+    async (msg) => {
+      if ((msg.body === MSG.prefix) || (msg.body === "P")) {
+        msg.reply(MSG.message.reply);
       }
-    }
-  },
+
+      const name = await msg.getContact();
+      if (MSG.logging.enabled === true) {
+        try {
+          LOG.info(`${name.number} (${name.name ? name.name : name.pushname}): ${msg.body}`);
+        } catch (err) {
+          LOG.error(err);
+        }
+      }
+    },
+  );
 };
