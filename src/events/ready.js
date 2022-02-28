4a6f9e79aacb859b2f3d93e3f9cf115ab6311dd0
ipincamp <support@nur-arifin.my.id>
2022-02-28 12:43:51 +0700

    Remodule event

--- src/events/ready.js
+++ src/events/ready.js
@@ -5,28 +5,14 @@
  * @license Apache-2.0
  */
 
-const client = require("../..");
+const { Client } = require("whatsapp-web.js");
 const { LOG } = require("../utils/Logger");
 const { MSG } = require("../utils/Messages");
 
-module.exports = {
-  name: "ready",
-  run() {
-    LOG.info(MSG.ready);
-
-    try {
-      const device = client.info.phone;
-
-      const info = `Hardware Information:
-            Manufacturer     : ${device.device_manufacturer}
-            Model            : ${device.device_model}
-            OS Build         : ${device.os_build_number}
-            OS Version       : ${device.os_version}
-            WhatsApp Version : ${device.wa_version}
-      `;
-      LOG.info(info);
-    } catch (err) {
-      LOG.error(err);
-    }
-  },
+/**
+ *
+ * @param {Client} client
+ */
+module.exports = function (client) {
+  client.on("ready", () => LOG.info(MSG.ready.success));
 };
